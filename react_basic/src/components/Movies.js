c48538aa95642a4251a7e7c9260ae7bfe5065235
RyuMinSu <sonicsu94@gmail.com>
2023-09-04 21:13:21 +0900

    react router settings

---
+++ react_basic/src/components/Movies.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+
+function Movie ({coverImage, title, summary, genres}) {
+	return (		
+		<div>
+		<img src={coverImage} alt="" />
+		<h2>{title}</h2>
+		<p>{summary}</p>
+		<ul>
+			{genres.map((genre) => (
+				<li key={genre}>
+					{genre}
+				</li>
+			))}
+		</ul>
+	</div>
+	)
+}
+
+
+Movie.propTypes = {
+	coverImag: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	summary: PropTypes.string.isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string.isRequired),
+}
+
+
+export default Movie
